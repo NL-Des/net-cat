02de85785a206e6c035918499dd4b3ec296177b0
nleonard <nathan.leonard.contact@gmail.com>
2025-09-08 13:22:50 +0200

    Mise en place de l'attribution d'un nom par l'utilisateur.

--- main.go
+++ main.go
@@ -1,55 +1,3 @@
-/* Lignes de commandes pour NC :
-
-OpenBSD netcat (Debian patchlevel 1.226-1ubuntu2)
-usage: nc [-46CDdFhklNnrStUuvZz] [-I length] [-i interval] [-M ttl]
-          [-m minttl] [-O length] [-P proxy_username] [-p source_port]
-          [-q seconds] [-s sourceaddr] [-T keyword] [-V rtable] [-W recvlimit]
-          [-w timeout] [-X proxy_protocol] [-x proxy_address[:port]]
-          [destination] [port]
-        Command Summary:
-                -4              Use IPv4
-                -6              Use IPv6
-                -b              Allow broadcast
-                -C              Send CRLF as line-ending
-                -D              Enable the debug socket option
-                -d              Detach from stdin
-                -F              Pass socket fd
-                -h              This help text
-                -I length       TCP receive buffer length
-                -i interval     Delay interval for lines sent, ports scanned
-                -k              Keep inbound sockets open for multiple connects
-                -l              Listen mode, for inbound connects
-                -M ttl          Outgoing TTL / Hop Limit
-                -m minttl       Minimum incoming TTL / Hop Limit
-                -N              Shutdown the network socket after EOF on stdin
-                -n              Suppress name/port resolutions
-                -O length       TCP send buffer length
-                -P proxyuser    Username for proxy authentication
-                -p port         Specify local port for remote connects
-                -q secs         quit after EOF on stdin and delay of secs
-                -r              Randomize remote ports
-                -S              Enable the TCP MD5 signature option
-                -s sourceaddr   Local source address
-                -T keyword      TOS value
-                -t              Answer TELNET negotiation
-                -U              Use UNIX domain socket
-                -u              UDP mode
-                -V rtable       Specify alternate routing table
-                -v              Verbose
-                -W recvlimit    Terminate after receiving a number of packets
-                -w timeout      Timeout for connects and final net reads
-                -X proto        Proxy protocol: "4", "5" (SOCKS) or "connect"
-                -x addr[:port]  Specify proxy address and port
-                -Z              DCCP mode
-                -z              Zero-I/O mode [used for scanning]
-        Port numbers can be individual or ranges: lo-hi [inclusive]*/
-
-/*
-Instructions :
--Pour lancer le serveur : go run main.go 8989
--Pour se connecter au serveur : nc 127.0.0.1 8989
-*/
-
 package main
 
 import (
@@ -57,6 +5,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -66,6 +15,7 @@ const (
 
 // Map pour stocker toutes les connexions actives
 var clients = make(map[net.Conn]bool)
+var userNames = make(map[net.Conn]string)
 var clientsMutex sync.Mutex
 
 // Origine et contenu du message.
@@ -116,7 +66,6 @@ func server() {
 	for {
 		connexions, err := ln.Accept()
 		gestionDesErreurs(err)
-		// go handleConnexion(connexions) // Doublons non nécessaire ?
 
 		fmt.Printf("Nouvelle connexion de : %s\n", connexions.RemoteAddr().String())
 
@@ -131,7 +80,9 @@ func server() {
 	}
 }
 
+// Gestion des connexions.
 func handleConnexion(connexions net.Conn) {
+
 	defer func() {
 		// Retirer le client de la liste quand il se déconnecte
 		clientsMutex.Lock()
@@ -141,17 +92,32 @@ func handleConnexion(connexions net.Conn) {
 		fmt.Printf("Client déconnecté : %s\n", connexions.RemoteAddr().String())
 	}()
 
-	byteMessage := make([]byte, 1024)
+	// Demande du nom de l'utilisateur.
+	_, err := connexions.Write([]byte("Bienvenue ! Veuillez saisir votre nom. \n"))
+	gestionDesErreurs(err)
 
+	// Lecture du nom de l'utilisateur.
+	nameBuffer := make([]byte, 1024)
+	name, err := connexions.Read(nameBuffer)
+	gestionDesErreurs(err)
+	userName := strings.TrimSpace(string(nameBuffer[:name]))
+	fmt.Printf("Pour %s, acquisition du nom : %s \n", connexions.RemoteAddr().String(), userName)
+
+	// Stockage des noms des utilisateurs.
+	clientsMutex.Lock()
+	userNames[connexions] = userName
+	clientsMutex.Unlock()
+
+	// Boucle de réception des messages.
+	byteMessage := make([]byte, 1024)
 	for {
 		n, err := connexions.Read(byteMessage)
 		if err != nil {
 			return // Le client s'est déconnecté
 		}
-
 		content := string(byteMessage[:n])
 		// Envoyer le message dans le canal pour diffusion
-		channels <- Message{ComeFrom: connexions.RemoteAddr().String(), Content: content}
+		channels <- Message{ComeFrom: userNames[connexions], Content: content}
 	}
 }
 
@@ -166,7 +132,7 @@ func messageHandler() {
 		for client := range clients {
 			_, err := client.Write([]byte(fmt.Sprintf("[%s]: %s", msg.ComeFrom, msg.Content)))
 			if err != nil {
-				// Si l'écriture échoue, supprimer le client
+				// Si l'écriture échoue, supprimer le client de la struct.
 				delete(clients, client)
 				client.Close()
 			}
